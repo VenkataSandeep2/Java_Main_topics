31b17f14a2e625132b064b22f5503fe0d1d68a10
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-23 21:32:17 +0530

    Update String1.java

--- Strings/String1.java
+++ Strings/String1.java
@@ -1,11 +1,12 @@
-// EMPTY String
+
 class String1
 {
 public static void main(String args[])
 	{
-	String s = new String();
-	System.out.println("The Object is "+s);
-	System.out.println("The Length of the String is"+s.length());
-	System.out.println("Is itEmpty"+s.isEmpty());
+	String s = new String("Sandeep");
+	System.out.println(s);
+	System.out.println("The Object is:  "+s);
+	System.out.println("The Length of the String is :"+s.length());
+	System.out.println("Is itEmpty :"+s.isEmpty());
 	}
 }
