788bc2c3ecf1b7d3267c41063e0f938c82409e47
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-03-27 23:06:18 +0530

    Create Exp8.java

---
+++ Exception Handling/Exp8.java
@@ -0,0 +1,18 @@
+package com.ExceptionHandling;
+
+class Exp8
+{
+public static void main(String args[])
+	{
+	Thread t = Thread.currentThread();
+	try{
+	t.start();
+	}catch(IllegalThreadStateException its)
+		{
+		System.out.println(its);
+		}
+
+	System.out.println();	
+	System.out.println("Hiiiiiiiii");
+	}
+}
