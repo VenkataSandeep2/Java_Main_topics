eeb3ef1870cc744d34f2016df950ad4243166656
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-04-06 20:34:51 +0530

    Create Exp_11.java

---
+++ Exception Handling/Exp_11.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.concurrent.*;
+public class Exp12 {
+
+	public static void main(String[] args) 
+	{
+        BlockingQueue<String> s = new ArrayBlockingQueue<String>(3);
+
+				try {
+		s.add("ravi");
+		s.add("ramu");
+		s.add("rabada");
+		s.add("raju");
+		
+				}
+				catch(IllegalStateException ie)
+				{
+					System.out.println(ie);
+				}
+				System.out.println("");
+		System.out.println("HIIIIIIIIIIII");
+	}
+
+}
