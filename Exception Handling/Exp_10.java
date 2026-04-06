94e2c44df937ce3dd1fdf903bbc34cc2ec686b34
Venkatasandeepadapala <sanjaysiva766@gmail.com>
2026-04-06 20:34:10 +0530

    Add exception handling for list clear operation

---
+++ Exception Handling/Exp_10.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class Exp_10 {
+
+	public static void main(String[] args) 
+	{	
+		String s[] = {"Ravi","Raju","Siva","Venky"};
+		List li =Arrays.asList(s);
+			try {
+		li.clear();
+		
+		System.out.println("Values of the list :- "+li);
+			}
+			catch(UnsupportedOperationException Uoe)
+			{
+				System.out.println(Uoe);
+			}
+		System.out.println("Hiiiiiiiii");
+	}
+
+}
